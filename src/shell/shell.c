db19ab032bd93a1ec202c8ab4fe0d93102859531
Leined18 <erdanielmarciano@hotmail.com>
2024-10-31 10:46:07 +0100

    fixed memory leak

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -6,7 +6,7 @@
 /*   By: danpalac <danpalac@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 12:29:32 by danpalac          #+#    #+#             */
-/*   Updated: 2024/10/31 10:21:58 by danpalac         ###   ########.fr       */
+/*   Updated: 2024/10/31 10:42:09 by danpalac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int	shell_loop(char **envp, pid_t pid)
 			continue ;
 		if (!process_input(input, env, pid))
 			return (free_null((void *)&input), 0);
+		free_null((void *)&input);
 		pause();
 	}
 }
@@ -47,10 +48,7 @@ int	process_input(char *input, char **envp, pid_t pid)
 		if (pid < 0)
 			return (ft_error("Failed to fork\n", 0), 0);
 		if (pid == 0)
-		{
 			execute_command(cmd);
-			free_command(&cmd);
-		}
 		free_command(&cmd);
 	}
 	else
